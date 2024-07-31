a717bf3b8b811ce4e7b5d40809e5f75840745fd3
Coders Club <Coders@club.com>
2024-07-31 16:27:26 +0300

    filter
    filter kısmı düzeltildi

--- src/app/ProductSection.tsx
+++ src/app/ProductSection.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { useSearchParams } from 'next/navigation';
 import React from 'react';
-import { convertStringToQueriesObject } from './FilterSection';
 import { products } from './products';
+import Image from 'next/image'; 
+import convertStringToQueriesObject from "./FilterSection"
+
 
 function isAvailable(arr1?: string[], arr2?: string[]): boolean {
   if (!arr1 || !arr2) {
@@ -10,8 +14,65 @@ function isAvailable(arr1?: string[], arr2?: string[]): boolean {
   return arr1.some((item) => arr2.includes(item));
 }
 
+
 const ProductSection = () => {
-  return <div>ProductSection</div>
-}
+  const searchParams = useSearchParams();
+  const paramsObj = convertStringToQueriesObject();
+
+  let filteredProducts = products.filter((product) => {
+    const hasCategories = isAvailable(product.types, paramsObj?.type);
+    const hasColors = isAvailable(product.color, paramsObj?.color);
+    const hasSize = isAvailable(product.size, paramsObj?.sizes);
+    return hasSize && hasColors && hasCategories;
+  });
+
+  if (paramsObj?.sort?.length > 0) {
+    filteredProducts = filteredProducts.sort((p1, p2) => {
+      switch (paramsObj.sort[0]) {
+        case 'Price: Low to High':
+          return p1.price - p2.price;
+        case 'Price: High to Low':
+          return p2.price - p1.price;
+        default:
+          return 0;
+      }
+    });
+  
+  }
+  if (Object.keys(paramsObj).length === 0) {
+    filteredProducts = products;
+  }
+  if (filteredProducts.length === 0) {
+    return <p className='text-center text-slate-700'>No products available</p>;
+  }
+
+  return (
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12'>
+      {filteredProducts.map((product) => (
+        <div key={product.id}>
+          <Image
+            src={product.image_url}
+            alt={product.brand}
+            width={300}
+            height={300}
+            className="rounded-md shadow-sm aspect-[4/5] object-cover object-top"
+          />
+          <div className='space-y-1'>
+            <div>
+              <p className='mt-4 font-medium truncate'>{product.brand}</p>
+            </div>
+            <p className='line-clamp-2 text-slate-500 text-sm'>
+              {product.color}
+            </p>
+            <div className='flex justify-between items-center'>
+              <p className='font-semibold'>Price: {product.price}</p>
+              <p className='flex gap-2 items-center mt-2'></p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductSection;
