d003499ef831703cd4922bb2e4a5332b666e2391
zisankarsatar <zisankarsatar@gmail.com>
2024-08-01 01:37:02 +0200

    mobil version added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -215,6 +215,9 @@ export default function Page() {
                                         name={`${section.id}[]`}
                                         type="checkbox"
                                         className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                        onClick={() =>
+                                          handleChange(option.label, section.id)
+                                        }
                                       />
                                       <label
                                         htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
